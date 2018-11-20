148fe5a17aabff3b285304589f51692290c7342c
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-11-20 03:12:26 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,10 @@ def reverse_each_word(str)
   arr.each {|word| rev.push(word.reverse)}
   rev.join(" ")
 end
+
+# def reverse_each_word(str)
+#   rev = []
+#   arr = str.split(" ")
+#   arr.each {|word| rev.push(word.reverse)}
+#   rev.join(" ")
+# end
